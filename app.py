a37749a8dc6ac2aa11b0c92a8c22ff638b22d69a
Corwin Hiatt <corwinhiatt@gmail.com>
2025-02-23 04:28:14 -0800

    Created a simple little quote app.

--- app.py
+++ app.py
@@ -1,12 +1,21 @@
-from flask import Flask
+from flask import Flask, render_template
+import random
 
 app = Flask(__name__)
 
+# List of fun quotes
+quotes = [
+    "I'm not lazy, I'm in energy saving mode.",
+    "Life is short. Smile while you still have teeth.",
+    "I don't need Google, my wife knows everything!",
+    "Why don't scientists trust atoms? Because they make up everything!",
+    "I used to think I was indecisive. But now I'm not so sure.",
+    "Don't worry if plan A fails, there are 25 more letters in the alphabet.",
+]
 
 @app.route('/')
-def hello_world():  # put application's code here
-    return 'Hello World!'
-
+def home():
+    return render_template('index.html', quote=random.choice(quotes))
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
